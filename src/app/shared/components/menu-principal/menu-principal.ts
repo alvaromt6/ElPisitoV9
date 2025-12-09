ddf17f06a3c073d9edfad42c1f84df713d9a858f
alvaromt6 <alvarito66614@gmail.com>
2025-12-10 00:45:25 +0100

    feat: agregamos la plantilla del menu principal

--- src/app/shared/components/menu-principal/menu-principal.ts
+++ src/app/shared/components/menu-principal/menu-principal.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-principal',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-principal.html',
   styleUrl: './menu-principal.css',
 })
