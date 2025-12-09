54497e7b6e18774cbf5f0bafbe7e8798eb7cc337
alvaromt6 <alvarito66614@gmail.com>
2025-12-10 00:07:29 +0100

    feat: creamos los componentes en shared para footer

---
+++ src/app/shared/components/menu-footer/menu-footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-footer',
+  imports: [],
+  templateUrl: './menu-footer.html',
+  styleUrl: './menu-footer.css',
+})
+export class MenuFooter {
+
+}
