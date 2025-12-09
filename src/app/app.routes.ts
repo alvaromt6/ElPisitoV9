a3e91a7a63623841d3430a953b0d1ee2306fdccc
alvaromt6 <alvarito66614@gmail.com>
2025-12-09 23:25:40 +0100

    feat: agregar rutas principales con lazy loading
    - Configuración de rutas para app, admin, auth y content
    - Lazy loading de módulos
    - Ruta comodín '**' para página de error
    -Creamos el componente error-general

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { ErrorGeneral } from './pages/content/error-general/error-general';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    {
+        path: '',
+        loadChildren: () => import('./pages/content/content.routes').then(m => m.CONTENT_ROUTES)
+    },
+    {
+        path: 'admin',
+        loadChildren: () => import('./pages/admin/admin.routes').then(m => m.ADMIN_ROUTES)
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./pages/auth/auth.routes').then(m => m.AUTH_ROUTES)
+    },
+    {
+        path: '**',
+        component:ErrorGeneral
+        /*ESTA RUTA SUENORE TIENE QUE ESTAR AL FINAL PARA QUE NO INTERFIERA CON LAS DEMAS RUTAS*/
+    },
+
+];
