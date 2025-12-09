cb2f236072f953e2743f7be99e01eee1e7b8efca
alvaromt6 <alvarito66614@gmail.com>
2025-12-10 00:31:10 +0100

    feat: agregamos los componentes al footer
    -Agregamos estilos al footer
    -importamos todos los componentes en el ts

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CabeceraFooter } from "../cabecera-footer/cabecera-footer";
+import { MenuFooter } from "../menu-footer/menu-footer";
+import { SocialMediaFooter } from "../social-media-footer/social-media-footer";
+import { PieFooter } from "../pie-footer/pie-footer";
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CabeceraFooter, MenuFooter, SocialMediaFooter, PieFooter],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
