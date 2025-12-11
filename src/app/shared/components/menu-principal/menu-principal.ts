158cab11f1c9617fc0ea2191b2fa84dc94c874d7
alvaromt6 <alvarito66614@gmail.com>
2025-12-11 10:32:46 +0100

    feat: implementar ngOnInit y ngOnDestroy en menu-principal.ts
    -Se suscribe al estado de login al iniciar el componente (ngOnInit).
    
    -Obtiene usuario y rol desde AuthService si está logueado.
    
    -Cancela la suscripción al salir del componente (ngOnDestroy) para evitar memory leaks.

--- src/app/shared/components/menu-principal/menu-principal.ts
+++ src/app/shared/components/menu-principal/menu-principal.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ComunicacionService } from '../../../core/services/comunicacion-service';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../../core/services/auth-service';
 
 @Component({
   selector: 'app-menu-principal',
@@ -7,6 +10,37 @@ import { RouterLink } from '@angular/router';
   templateUrl: './menu-principal.html',
   styleUrl: './menu-principal.css',
 })
-export class MenuPrincipal {
+export class MenuPrincipal implements OnInit, OnDestroy{
+  /*
+    -Se suscribe al estado de login al iniciar el componente (ngOnInit).
+
+    -Obtiene usuario y rol desde AuthService si está logueado.
+
+    -Cancela la suscripción al salir del componente (ngOnDestroy) para evitar memory leaks.
+  */
+
+  estaLogueado: boolean;
+  suscripcion: Subscription;
+  usuario: string;
+  rol: string;
+  private _comunicacionService: ComunicacionService = inject(ComunicacionService);
+  private _authService: AuthService = inject(AuthService);
+
+  ngOnInit(): void {
+    this.suscripcion = 
+      this._comunicacionService.estadoLogin$.subscribe({
+        next: (estado) => {
+          this.estaLogueado = estado
+          if(this.estaLogueado){
+            this.usuario = this._authService.getUsuarioFromToken();
+            this.rol = this._authService.getRolFromToken();
+          }
+        }
+      })
+  }
+  
+  ngOnDestroy(): void {
+    this.suscripcion.unsubscribe();
+  }
 
 }
