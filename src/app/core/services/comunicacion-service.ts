51cdf503666aa45abb3d8a7b7d68c9c8028815a8
alvaromt6 <alvarito66614@gmail.com>
2025-12-11 10:18:26 +0100

    feat: agregar ComunicacionService para manejo de estado de login

--- src/app/core/services/comunicacion-service.ts
+++ src/app/core/services/comunicacion-service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AuthService } from './auth-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ComunicacionService {
+
+  /*
+  _authService utilizamos _ para las variables que inyectan en el codigo
+
+  estadoLogueo$ utilizamos $ para indicar que es una variable observable
+
+  */
+
+  private _authService:AuthService = inject(AuthService);
+
+  estadoLogin:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this._authService.estaLogueado());
+
+  estadoLogin$: Observable<boolean> = this.estadoLogin.asObservable(); //hot
+
+  cambioLogin(nuevoEstado: boolean): void{
+    this.estadoLogin.next(nuevoEstado) //.next es un método de BehaviorSubject
+
+  }
+
+
   
 }
