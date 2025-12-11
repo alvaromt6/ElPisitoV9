ee6358ef26607b52af1699abd9c099f069ee965f
alvaromt6 <alvarito66614@gmail.com>
2025-12-11 19:41:17 +0100

    feat: creamos el método para hacer logout, importamos Router para redirigir al usuario cuando haga login o logout
    login:  Redirigimos al usuario a "/"
    
    logout: Redirigimos al usuario a "/" y dejamos las variables vacias

--- src/app/shared/components/menu-principal/menu-principal.ts
+++ src/app/shared/components/menu-principal/menu-principal.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ComunicacionService } from '../../../core/services/comunicacion-service';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../../core/services/auth-service';
@@ -25,22 +25,33 @@ export class MenuPrincipal implements OnInit, OnDestroy{
   rol: string;
   private _comunicacionService: ComunicacionService = inject(ComunicacionService);
   private _authService: AuthService = inject(AuthService);
+  private _router:Router = inject(Router);
 
   ngOnInit(): void {
-    this.suscripcion = 
-      this._comunicacionService.estadoLogin$.subscribe({
+    this.suscripcion = this._comunicacionService.estadoLogin$.subscribe({
         next: (estado) => {
-          this.estaLogueado = estado
+          this.estaLogueado = estado;
+
           if(this.estaLogueado){
             this.usuario = this._authService.getUsuarioFromToken();
             this.rol = this._authService.getRolFromToken();
+            this._router.navigate(["/"]); //Al hacer login mandamos al usuario a "/"
           }
         }
-      })
+      });
   }
   
   ngOnDestroy(): void {
     this.suscripcion.unsubscribe();
+  }
+
+  logout(): void{
+    this._authService.logout();
+    this._comunicacionService.cambioLogin(false);
+    //Al hacer logout mandamos al usuario a "/" y dejamos las variables vacias
+    this._router.navigate(["/"]);
+    this.usuario = "";
+    this.rol = "";
   }
 
 }
