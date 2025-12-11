dfcfd445a1fa8f9c1d02f0e920d0a1ab3d58a1be
alvaromt6 <alvarito66614@gmail.com>
2025-12-11 19:59:16 +0100

    feat: agregamos las rutas en el archivo content.routes.ts

--- src/app/pages/content/content.routes.ts
+++ src/app/pages/content/content.routes.ts
@@ -1,4 +1,11 @@
+import { ConsultaHipotecas } from "./consulta-hipotecas/consulta-hipotecas";
+import { Contactar } from "./contactar/contactar";
 import { Home } from "./home/home";
+import { MapaWeb } from "./mapa-web/mapa-web";
+import { NuestrosServicios } from "./nuestros-servicios/nuestros-servicios";
+import { PublicaTuAnuncio } from "./publica-tu-anuncio/publica-tu-anuncio";
+import { RegistroUsuario } from "./registro-usuario/registro-usuario";
+import { SobreElPisito } from "./sobre-el-pisito/sobre-el-pisito";
 
 export const CONTENT_ROUTES = [
 
@@ -9,5 +16,36 @@ export const CONTENT_ROUTES = [
     {
         path:'home',
         component: Home
-    }
+    },
+    {
+        path:'publica-anuncio',
+        component: PublicaTuAnuncio
+    },
+    {
+        path:'consulta-hipoteca',
+        component: ConsultaHipotecas
+    },
+    {
+        path:'nuestros-servicios',
+        component: NuestrosServicios
+    },
+    {
+        path:'sobre-el-pisito',
+        component: SobreElPisito
+    },
+    {
+        path:'contactar',
+        component: Contactar
+    },
+    {
+        path:'mapa-web',
+        component: MapaWeb
+    },
+    {
+        path:'registro-usuario',
+        component: RegistroUsuario
+    },
+    
+    
+
 ];
