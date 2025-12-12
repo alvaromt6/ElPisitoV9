d65a1152e45a5ada51e41fed9a21f72332eb1df1
alvaromt6 <alvarito66614@gmail.com>
2025-12-12 11:39:50 +0100

    feat: crear interceptor de errores HTTP con almacenamiento en ErrorStoreService

--- src/app/pages/content/error-general/error-general.ts
+++ src/app/pages/content/error-general/error-general.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ErrorStoreService } from '../../../core/services/error-store-service';
 
 @Component({
   selector: 'app-error-general',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './error-general.html',
   styleUrl: './error-general.css',
 })
-export class ErrorGeneral {
+export class ErrorGeneral implements OnInit{
+
+  status: number;
+  message: string;
+  
+  private _errorStoreService:ErrorStoreService = inject(ErrorStoreService);
+
+  ngOnInit(): void {
+
+    this.status = this._errorStoreService._errorStatus();
+    this.message = this._errorStoreService._errorMessage();
+  }
+
+
 
 }
