548e0af441d26c8f2c3576797aff1055c8fea790
alvaromt6 <alvarito66614@gmail.com>
2025-12-14 23:26:51 +0100

    feat: agregar interfaz UsuarioDataYState para gestionar el estado de login

--- src/app/core/models/auxiliars.ts
+++ src/app/core/models/auxiliars.ts
@@ -9,3 +9,10 @@ export interface CustomJwtPayload extends JwtPayload{
     ID:number;
 
 }
+
+export interface UsuarioDataYState{
+    //Para no tener que crear un objeto vacio inicializamos las variables con ?
+    logueado?: boolean;
+    id?: number;
+    rol?: string;
+}
