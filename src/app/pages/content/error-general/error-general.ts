9b5b6d43cede989bda5c616bd11b8c50e207dada
alvaromt6 <alvarito66614@gmail.com>
2025-12-15 16:48:25 +0100

    feat: añadir a errorGeneral una página para cuando status = 401
    -Cuando nos de este error desconectara al usuario y le mostrara un botón para iniciar sesión

--- src/app/pages/content/error-general/error-general.ts
+++ src/app/pages/content/error-general/error-general.ts
@@ -1,5 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ErrorStoreService } from '../../../core/services/error-store-service';
+import { AuthService } from '../../../core/services/auth-service';
+import { Router } from '@angular/router';
+import { ComunicacionService } from '../../../core/services/comunicacion-service';
 
 @Component({
   selector: 'app-error-general',
@@ -12,14 +15,25 @@ export class ErrorGeneral implements OnInit{
   status: number;
   message: string;
   
+  private _router: Router = inject(Router);
+  private _authService: AuthService = inject(AuthService);
+  private _comunicacionService: ComunicacionService = inject(ComunicacionService);
   private _errorStoreService:ErrorStoreService = inject(ErrorStoreService);
 
   ngOnInit(): void {
 
     this.status = this._errorStoreService._errorStatus();
     this.message = this._errorStoreService._errorMessage();
+    
+    if(this.status === 401){
+      this._authService.logout();
+      this._comunicacionService.cambioLogin(false);
+    }
   }
 
+  login():void{
+    this._router.navigate(['/auth/login']);
+  }
 
 
 }
