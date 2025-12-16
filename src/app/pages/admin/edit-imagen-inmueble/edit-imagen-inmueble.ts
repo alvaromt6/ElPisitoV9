672d99afb5b930642a3c7e77d691616ba762824a
alvaromt6 <alvarito66614@gmail.com>
2025-12-16 12:46:24 +0100

    feat: componentes 'edit' para admin

---
+++ src/app/pages/admin/edit-imagen-inmueble/edit-imagen-inmueble.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-imagen-inmueble',
+  imports: [],
+  templateUrl: './edit-imagen-inmueble.html',
+  styleUrl: './edit-imagen-inmueble.css',
+})
+export class EditImagenInmueble {
+
+}
